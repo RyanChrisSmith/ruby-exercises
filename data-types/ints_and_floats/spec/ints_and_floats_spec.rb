4aa594a273b656a8becd23b26b1ba8a6fd505696
Ryan Smith <ryan.chris.smith@gmail.com>
2022-07-05 08:21:14 -0600

    finished exercises

--- data-types/ints_and_floats/spec/ints_and_floats_spec.rb
+++ data-types/ints_and_floats/spec/ints_and_floats_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe 'ints and floats' do
     expect(sum).to eq(20)
   end
 
-  xit 'test 2' do
+  it 'test 2' do
     lucky = 7
     unlucky = 13
     # Using the two variables defined above,
@@ -17,26 +17,26 @@ RSpec.describe 'ints and floats' do
     expect(difference).to eq(-6)
   end
 
-  xit 'test 3' do
+  it 'test 3' do
     lucky = 7
     unlucky = 13
     # Using the two variables defined above,
     # divide unlucky by lucky
     # NOTE: this is integer division
-    quotient = lucky / unlucky
+    quotient = unlucky / lucky
     expect(quotient).to eq(1)
   end
 
-  xit 'test 4' do
+  it 'test 4' do
     lucky = 7
     unlucky = 13
     # Using the two variables defined above,
     # divide unlucky by lucky
-    quotient = lucky / unlucky
+    quotient = unlucky / lucky.to_f
     expect(quotient).to eq(1.8571428571428572)
   end
 
-  xit 'test 5' do
+  it 'test 5' do
     lucky = 7
     unlucky = 13
     # Using the two variables defined above,
@@ -45,15 +45,15 @@ RSpec.describe 'ints and floats' do
     expect(remainder).to eq(6)
   end
 
-  xit 'test 6' do
+  it 'test 6' do
     lucky = 7
     # Using the variable defined above,
     # find out if the lucky number is even
-    even = unlucky.even?
+    even = lucky.even?
     expect(even).to eq(false)
   end
 
-  xit 'test 7' do
+  it 'test 7' do
     pi = 3.14
     # Using the variable defined above,
     # round the number to the nearest whole number
@@ -61,7 +61,7 @@ RSpec.describe 'ints and floats' do
     expect(rounded).to eq(3)
   end
 
-  xit 'test 8' do
+  it 'test 8' do
     pi = 3.14
     # Using the variable defined above,
     # round the number to one decimal place
@@ -69,7 +69,7 @@ RSpec.describe 'ints and floats' do
     expect(rounded).to eq(3.1)
   end
 
-  xit 'test 9' do
+  it 'test 9' do
     pi = 3.14
     # Using the variable defined above,
     # round the number to the next highest whole number
